a2a5830061da357b0c85b326ac8921c9b2051a1e
Aysuarex <aysuarex@gmail.com>
2022-06-01 16:30:25 +0100

    Adding sleep function

--- Tictactoe.c
+++ Tictactoe.c
@@ -8,3 +8,17 @@ int checkWin();
 char position[9]= {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 int turn;
 
+int main()
+{
+    char mark;
+    int choice;
+    int player;
+    int Player_1, Player_2;
+    unsigned int turn = 1;
+
+    system("cls");
+    printf("Welcome to TICTACTOE!\n");
+    Sleep(800);
+    drawBoard();
+
+   
