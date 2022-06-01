4385760c7dfac24630aed1fc4c44408d1e3c05c4
Aysuarex <aysuarex@gmail.com>
2022-06-01 16:50:24 +0100

    Fix: Adjusted the board placement

--- Tictactoe.c
+++ Tictactoe.c
@@ -21,7 +21,7 @@ int main()
     Sleep(800);
     drawBoard();
 
- Sleep(1000);
+    Sleep(1000);
     Mark: //Reference Point
     
     printf("-----------------------------------------------\n");
@@ -50,7 +50,6 @@ int main()
         player = 1;
 
     //drawBoard();
-}*/
 
     Position: //Reference Point
     drawBoard();
@@ -107,31 +106,4 @@ void drawBoard()
 
 
     return 0;
-        goto Position;
-    }
-    //drawBoard();
-    goto Next;
-    return 0;
-}
-
-void drawBoard()
-{
-    printf("\t\t\t\t\t\t      |      |      \n");
-    printf("\t\t\t\t\t\t   %c  |   %c  |  %c  \n", position[0], position[1], position[2]);
-    printf("\t\t\t\t\t\t______|______|______\n");
-    printf("\t\t\t\t\t\t      |      |      \n");
-    printf("\t\t\t\t\t\t   %c  |   %c  |  %c  \n", position[3], position[4], position[5]);
-    printf("\t\t\t\t\t\t______|______|______\n");
-    printf("\t\t\t\t\t\t      |      |      \n");
-    printf("\t\t\t\t\t\t   %c  |   %c  |  %c  \n", position[6], position[7], position[8]);
-    printf("\t\t\t\t\t\t      |      |      \n");
-
-    return;
-}
-
-/*int checkWIn()
-{
-
-
-
-    return 0;
+}*/
