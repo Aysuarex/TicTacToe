fb9870c0e1cde61bb7a32d5b9604a707402bc012
Aysuarex <aysuarex@gmail.com>
2022-06-01 16:46:38 +0100

    Including drawboard Function

--- Tictactoe.c
+++ Tictactoe.c
@@ -68,4 +68,24 @@ int main()
         position[3] = mark;
     else if (choice ==5 && position[4] == '5')
         position[4] = mark;
-   
+    else if (choice ==6 && position[5] =='6')
+        position[5] = mark;
+    else if (choice ==7 && position[6] =='7')
+        position[6] = mark;
+    else if (choice ==8 && position[7] == '8')
+        position[7] = mark;
+    else if (choice ==9 && position[8] =='9') 
+        position[8] = mark;
+    else {
+        printf("ERROR! Invalid Option\n");
+        --turn;
+        goto Position;
+    }
+    //drawBoard();
+    goto Next;
+    return 0;
+}
+
+void drawBoard()
+{
+    printf("\t\t\t\t\t\t      |      |      \n");
