24a5e9fbca9c1627adc4c31f2cb291747eb65c55
Aysuarex <aysuarex@gmail.com>
2022-06-07 14:00:14 +0100

    Re-compiled .exe file after update

--- Tictactoe.c
+++ Tictactoe.c
@@ -78,7 +78,7 @@ int main()
         turn--;
         goto Position;
     }
-    checkWin()
+    checkWin();
     //drawBoard();
     goto Next;
     return 0;
@@ -101,26 +101,26 @@ void drawBoard()
 
 int checkWIn()
 {
-    if position[0] == position[1] == position [2]
+    if (position[0] == position[1] == position [2])
         return 1;
-    else if position[3] == position[4] == position[5]
+    else if (position[3] == position[4] == position[5])
         return 1;
-    else if position[6] == position[7] == position[8]
+    else if (position[6] == position[7] == position[8])
         return 1;
-    else if position[0] == position[4] == position[8]
+    else if (position[0] == position[4] == position[8])
         return 1;
-    else if position[0] == position[3] == position[6]
+    else if (position[0] == position[3] == position[6])
         return 1;
-    else if position[1] == position[4] == position[7]
+    else if (position[1] == position[4] == position[7])
         return 1;
-    else if position[2] == position[5] == position[8]
+    else if (position[2] == position[5] == position[8])
         return 1;
-    else if position[2] == position[4] == position[6]
+    else if (position[2] == position[4] == position[6])
         return 1;
-    else if position[0] != '1' && position[1] != '2' && position[2] != '3' && position[3] != '4' && position[4] != '5' && position[5] != '6' && position[6] != '7' && position[7] != '8' && position[8] != '9'
+    else if (position[0] != '1' && position[1] != '2' && position[2] != '3' && position[3] != '4' && position[4] != '5' && position[5] != '6' && position[6] != '7' && position[7] != '8' && position[8] != '9')
         return 0;
     else 
-        return -1
+        return -1;
 }
 
 
