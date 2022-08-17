b01ca5fad1996a4701974fd62aaa294f6ddc1f8e
Aysuarex <aysuarex@gmail.com>
2022-08-17 23:47:17 +0100

    Header file for prototypes

--- main.h
+++ main.h
@@ -1,1 +1,21 @@
-Header file
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+
+//prototypes for multiplayer mode
+void human();
+void drawBoard();
+int checkWin();
+
+//prototypes for single player mode
+void computer();
+void computerMove();
+void playerMove();
+int resetBoard();
+void printBoard();
+char checkWinner();
+void printWinner();
+int checkFreeSpaces();
+
+
+#endif
