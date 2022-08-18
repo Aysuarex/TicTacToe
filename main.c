cec6de484c5923a4b6a7feda066adaf05b431e43
Aysuarex <aysuarex@gmail.com>
2022-08-18 19:34:30 +0100

    Updatede Driver file

--- main.c
+++ main.c
@@ -2,27 +2,33 @@
 #include <windows.h>
 #include "main.h"
 
-//char position[9]= {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
 char choice;
 int main()
 {
     START:
     system("cls");
-    printf("\n==============================\n");
+    printf("\n============================\n");
     printf("Welcome to TICTACTOE\n");
+    printf("============================\n");
     BEGIN:
     Sleep(500);
-    printf("==============================\n");
+    printf("\n============================\n");
     printf("How do you wish to play?\n");
     printf("A. Against the computer\n");
-    printf("B. Against a friend\n\t\t==> ");
+    printf("B. Against a friend\n\n");
+    printf("X. Exit Game\n\t\t==> ");
     scanf("%s", &choice);
 
     if (choice == 'A' || choice == 'a')
         computer();
     else if (choice == 'B' || choice == 'b')
         human();
+    else if (choice == 'X' || choice == 'x')
+    {
+        printf("\nBYE! Come Back Soon\n");
+        Sleep(2000);
+        exit(0);
+    }
     else 
         printf("ERROR! Invalid Option\n\n");
         goto BEGIN;
