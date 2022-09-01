b4298d83363f57818e673a1edaceebe487ee344b
Aysuarex <aysuarex@gmail.com>
2022-09-01 15:28:05 +0100

    main driver file for game launch

--- Tictactoe-in-Python/tictactoe.py
+++ Tictactoe-in-Python/tictactoe.py
@@ -1,113 +1,82 @@
 import time
+import os
 from player import HumanPlayer, RandomComputerPlayer
+from gameplay import TicTacToe
 
 """
 Tictactoe game using OOP
 """
 
-class TicTacToe:
-    def __init__ (self):
-        self.board = [' ' for _ in range(9)] #list to represent 3x3 board
-        self.current_winner = None  #keeps track of whether 
-                                    # there has been a winner, and who it was.
-    
-    def print_board (self): #an empty board
-        for row in [self.board[i*3 : (i+1)*3] for i in range(3)]:
-            print('| ' + ' | '.join(row) + ' |')
-
-    @staticmethod
-    def print_board_nums(): #board with placeholder
-        # now we determine which number corresponds with which spot
-        # e.g | 0 | 1 | 2 |
-        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
-        for row in number_board:
-            print('| ' + ' | '.join(row) + ' |')
-    
-    # To know the available moves after every turn
-    def available_moves(self): #returns a list of indices of the available spots
-        return [i for i,spot in enumerate(self.board) if spot == ' ']
-
-        # This list comprehension basically expands to the below:
-        #moves = []
-        #for (i,spot) in enumerate(self.board):
-        #    if spot == ' '
-        #        moves.append(i)
-        
-    def empty_spots(self): #checks if there ar empty spots on the board
-        return ' ' in self.board
-        #boolean that returns true if there are free spots
-        
-    def num_empty_spots(self): #returns the number of available moves left on board
-        return len(self.available_moves())
-        # this can also be done by:
-        #return self.board.count(' ')
-
-    #checks if the game has been won and defines how it happens
-    def winner(self, spot, token):
-    
-        #check rows
-        row_index = spot // 3   # all 3 elements along the same row 
-                                # should have the same whole number 
-                                # division of 3 (i.e 0//3 == 1//3 == 2//3 == 0) 
-        row = self.board[row_index*3 : (row_index+1)*3]
-        #given the index, get the row i.e print('row', row)
-        if all([r==token for r in row]) #all spots in a row have a particular token
-            return True
-
-        #check columns
-        col_index = spot % 3    # all 3 elements along the same column
-                                # should have the same value of modulus 3 
-                                # (i.e 0%3 == 3%3 == 6%3 == 0) 
-        column = [self.board[col_index + i*3] for i in range(3)] #print('col', col)
-        if all ([c==token for c in column]):
-            return True
-
-        #checks diagonals
-        #for diagonal
-        #
-        
-         s
-
-
-
-
-    def make_move(self, spot, token):
-        #if the move is valid, make the move and return true
-        if self.board[spot] == ' '   # if spot on board is empty
-            self.board[spot] = token # then assign the next token there
-
-            #check if someone won the game after their turn
-            if self.winner(spot, token):    # if the game has been won,
-                self.current_winner = token # The current_winner takes the token
-                                            # of the player that played last
-            return True
-        return False
-
-
-
-
-
-def play(game, x_player, o_player, print_game=True):
+def main(game, x_player, o_player, print_game=True):
     if print_game:              #if print_game is set to True
         game.print_board_nums() #then print the game board with 
-                                #the placeholders within
+        time.sleep(.8)          #the placeholders within
     token = 'X' #the default first player is X
 
     # as long as there are still empty spots on the board, the 
     # player corresponding to the appropriate token is assigned
-    while game.empty_slots():
-        if token = 'X':
+    while game.empty_spots():
+        if token == 'X':
             spot = x_player.get_move(game)
-        else:
+        else: #spot=x_player.get_move(game) if token=='X' else o_player.get_move(game)
             spot = o_player.get_move(game)
 
         if game.make_move(spot, token):
+            if print_game:
+                print(f"{token} makes a move to spot {spot+1}") #this will print 1-9 instead of 0-8
+                game.print_board()
+                print('\n')
+            
+            if game.current_winner: # if the current_winner has been set to a token
+                                    # since current_winner was initialized as None.
+                if print_game:
+                    print(f"{token} Wins!")
+                return token #ends the loop and exits the game
+            #alternate the tokens, switching the player who plas next
+            token = 'O' if token == 'X' else 'X'
+            
+        time.sleep(0.8) #a small break to make things easier to read
+
+    if print_game:
+        print("It's a tie!")
 
 
+if __name__ == "__main__":
+    os.system("cls")
+    print("\nWelcome to Tic-Tac-Toe!")
+    replay = 'Y' #initialised for while loop
+    while replay == 'Y':
+        mode = ' ' #initialised for while loop
+        while mode != 'A' and mode != 'B' and mode != 'C':
+            print("\nHow do you wish to play?")
+            print("A. Player against Computer")
+            print("B. Player_1 against Player_2")
+            print("C. Computer_1 against Computer_2")
+            mode = input("\t\t\t\t==> ").upper()
 
-
-if __name__ = "__main__":
-    x_player = HumanPlayer('X')
-    o_player = RandomComputerPlayer('O')
-    t = TicTacToe()
-    play(t, x_player, o_player, print_game=True)
+            if mode == 'A':
+                choice = input("\nOK... Do you want to be X or O? ").upper()
+                if choice == 'O':
+                    o_player = HumanPlayer('O')
+                    x_player = RandomComputerPlayer('X')
+                else:
+                    x_player = HumanPlayer('X')
+                    o_player = RandomComputerPlayer('O')
+            elif mode == 'B':
+                print("Player_1 = X\nPlayer_2 = O\n")
+                x_player = HumanPlayer('X')
+                o_player = HumanPlayer('O')
+            elif mode == 'C':
+                print("\nSit Back, Relax and Enjoy the Show :)")
+                time.sleep(2.5)
+                x_player = RandomComputerPlayer('X')
+                o_player = RandomComputerPlayer('O')
+            else:
+                print("Invalid choice. Try again")
+        #end of while loop
+        t = TicTacToe()
+        main(t, x_player, o_player, print_game=True)
+        replay = input("\nDo you want to play again?(Y/N): ").upper()
+        os.system("cls")
+    #end of while loop
+    print("Bye! Come Back Soon")
